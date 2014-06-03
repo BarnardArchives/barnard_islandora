0f399f90e7816ab85a368e3c24997e056802833b
Dillon Savage <dillon.savage@gmail.com>
2014-06-03 17:16:54 -0400

    a plethora of adjustments

--- js/bc_islandora.js
+++ js/bc_islandora.js
@@ -2,67 +2,127 @@
   $(document).ready(function () {
     var bulletin_calendar = {
         hide_lists: function () {
-            $('.bulletin-calendar ul').each(function() {
-                if ($(this).parent().attr('class') != 'years') {
-                    $(this).addClass('inactive');
-                }
-                else {
-                    $(this).addClass('active');
-                }
-            });
+          $('.bulletin-calendar ul').each(function() {
+            if ($(this).parent().attr('class') != 'years') {
+              $(this).addClass('inactive');
+            }
+            else {
+              $(this).addClass('active');
+            }
+          });
         },
         listeners: function () {
-            var self = this;
-            // year click
-            $('.years li.year a').click(function () {
-                var year = $(this).parent().attr('id');
-                console.log('got year: ' + year);
-                var months = $('.months-' + year);
-                self.toggle_list('ul.years', '.months-' + year);
-                self.nav_display('.year #nav', year);
-                self.nav_display('.year', year);
-                self.nav_display('.browse', '<a href="#" id="nav">Browse</a>&nbsp;&gt;&nbsp;');
-            });
-            // month click
-            $('.months li.month a').click(function () {
-                var year = $('.bulletin-nav .year').text();
-                var month = $(this).parent().attr('id');
-                var month_text = $(this).text();
-                var issues_selector = '.issues-' + year + '-' + month;
-                self.toggle_list('.months-' + year, issues_selector);
-                self.nav_display('.month', month_text + ', ');
-                self.nav_display('.year', '<a href="#">' + year + '</a>');
-            });
-            // nav year click
-            $('.bulletin-nav .year').click(function () {
-                var active_list = $('.bulletin-calendar').find('ul.active');
-                var month_list = $('ul.months-' + $(this).text());
-                self.toggle_list(active_list, month_list);
-                self.nav_display('.month', '');
-            });
-            // nav browse click
-            $('.bulletin-nav .browse').click(function() {
-                console.log('got click');
-                var active_list = $('.bulletin-calendar').find('ul.active');
-                self.toggle_list(active_list, $('.years ul.years'));
-                self.nav_display('.year', '');
-                self.nav_display('.month', '');
-                self.nav_display('.browse', 'Browse');
-            });
-        },
-        nav_display: function (selector, text) {
-            $('.bulletin-nav').find(selector).html(text);
-        },
-        toggle_list: function (from_selector, to_selector) {
-            $(from_selector).removeClass('active').addClass('inactive');
-            $(to_selector).removeClass('inactive').addClass('active');
-        },
-        run: function () {
-            this.hide_lists();
-            this.listeners();
-        }
+          var self = this;
+          // year click
+          $('.years li.year a').click(function () {
+            var year = $(this).parent().attr('id');
+            console.log('got year: ' + year);
+            var months = $('.months-' + year);
+            self.toggle_list('ul.years', '.months-' + year);
+            self.nav_display('.year #nav', year);
+            self.nav_display('.year', year);
+            self.nav_display('.browse', '<a href="#" id="nav">Browse</a>&nbsp;&gt;&nbsp;');
+          });
+          // month click
+          $('.months li.month a').click(function () {
+            var year = $('.bulletin-nav .year').text();
+            var month = $(this).parent().attr('id');
+            var month_text = $(this).text();
+            var issues_selector = '.issues-' + year + '-' + month;
+            self.toggle_list('.months-' + year, issues_selector);
+            self.nav_display('.month', month_text + ', ');
+            self.nav_display('.year', '<a href="#">' + year + '</a>');
+          });
+          // nav year click
+          $('.bulletin-nav .year').click(function () {
+            var active_list = $('.bulletin-calendar').find('ul.active');
+            var month_list = $('ul.months-' + $(this).text());
+            self.toggle_list(active_list, month_list);
+            self.nav_display('.month', '');
+          });
+          // nav browse click
+          $('.bulletin-nav .browse').click(function() {
+            console.log('got click');
+            var active_list = $('.bulletin-calendar').find('ul.active');
+            self.toggle_list(active_list, $('.years ul.years'));
+            self.nav_display('.year', '');
+            self.nav_display('.month', '');
+            self.nav_display('.browse', 'Browse');
+          });
+      },
+      nav_display: function (selector, text) {
+          $('.bulletin-nav').find(selector).html(text);
+      },
+      toggle_list: function (from_selector, to_selector) {
+        $(from_selector).removeClass('active').addClass('inactive');
+        $(to_selector).removeClass('inactive').addClass('active');
+      },
+      run: function () {
+        this.hide_lists();
+        this.listeners();
+      }
+    };
+    
+    var exhibition = {
+      show_hide_all: function () {
+        $('.exhibition-object').each(function() {
+          $(this).addClass('inactive');
+        })
+        $('.exhibition-object:first').removeClass('inactive').addClass('active');
+      },
+      css_positioning: function () {
+        $('.exhibition-object').each(function() {
+          var $img = $(this).find('img'), img = new Image();
+          var $caption = $(this).find('span.caption');
+          img.onload = function() {
+            var caption_left = (960 - $img.attr('width'))/2 + 'px';
+            $caption.css('margin-left', caption_left);
+          }
+          img.src = $img.attr('src');
+        });
+      },
+      listeners: function () {
+        $('.exhibition-next a').click(function(event) {
+          event.preventDefault();
+          var $active = $('.exhibition').find('.active');
+          var $next = $active.next();
+          var $first = $('.exhibition').children().first();
+          $active.removeClass('active').addClass('inactive');
+          if ($next.length != 0) {
+            $next.removeClass('inactive').addClass('active');
+          }
+          else {
+            $first.removeClass('inactive').addClass('active');
+          }
+        });
+        $('.exhibition-prev a').click(function(event) {
+          event.preventDefault();
+          var $active = $('.exhibition').find('.active');
+          var $prev = $active.prev();
+          var $last = $('.exhibition').children().last();
+          $active.removeClass('active').addClass('inactive');
+          if ($prev.length != 0) {
+            $prev.removeClass('inactive').addClass('active');
+          }
+          else {
+            $last.removeClass('inactive').addClass('active');
+          }
+        });
+      },
+      run: function () {
+        this.show_hide_all();
+        this.css_positioning();
+        this.listeners();
+      }
     };
 
-    bulletin_calendar.run();
+    // TODO (yuk)
+    if (window.location.href.indexOf('bulletin') !== -1) {
+      bulletin_calendar.run();
+    }
+
+    if (window.location.href.indexOf('exhibitions') !== -1) {
+      exhibition.run();
+    }
   });
 }) (jQuery);
