1c7afae41fa013dab7bf07227cf3725254ff473d
Benjamin Rosner <brosner@barnard.edu>
2017-10-16 12:03:37 -0400

    Barnard Islandora Scrapbook Development
    This update:
    - rewrite the compound javascript significantly
     - removed unused properties.
     - reorgnization and renaming.
     - refactored name to something more fitting.
    - added ability to set initial display modes for books displayed in IIAB.
    - minor theme updates. the focus was still on function to this point.
    
    blearly,
    ben

--- js/barnard_islandora_internet_archive_bookreader.js
+++ js/barnard_islandora_internet_archive_bookreader.js
@@ -2,29 +2,22 @@
  * @file
  * Defines initializing/attaching the Book Reader to the
  * defined element.
+ *
+ * This bookreader file is modified from its original form.
+ * Purpose: redefines the BookReader proto to be aware
+ * of Islandora Compound Objects.
  */
 
 (function ($) {
     Drupal.behaviors.islandoraInternetArchiveBookReader = {
         attach: function (context, settings) {
-            $('.barnard--islandora-internet-archive-bookreader', context).once('islandora-bookreader', function () {
+            $('.islandora-internet-archive-bookreader', context).once('islandora-bookreader', function () {
                 var bookReader = new IslandoraBookReader(settings.islandoraInternetArchiveBookReader);
-                // Sanity operations for compound objects.
-                bookReader.compoundInit();
                 // Initialize and Render the BookReader.
                 bookReader.init();
 
-                if (bookReader.isOnCompound()) {
-                    bookReader.switchToCompoundObjectDisplay();
-                }
-
-                // Hide unused or unwanted toolbar buttons from view.
+                  // Hide unused or unwanted toolbar buttons from view.
                 $('#BRtoolbar').find('.read, .info, .share, .play, .pause').hide();
-
-                // fixes odd zooming issues on large resolution inclusions.
-                // if (bookReader.isOnInclusion) {
-                //     // bookReader.zoom(-1);
-                // }
 
                 if (!bookReader.searchEnabled()) {
                     $('#textSrch').hide();
